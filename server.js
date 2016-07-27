7c2c059116f37441f26a9e5ad5bd8e4f75468438
Alex Reinking <alex_reinking@yahoo.com>
2016-07-26 21:14:38 -0500

    server update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
-var PORT = 3000 || process.env.PORT;
+var PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
